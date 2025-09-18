72f55978ec97a0e11909d1d7242b0629ad808518
Edinson Jim <4842086+edinsonjim@users.noreply.github.com>
2025-09-19 01:36:57 +0200

    feat: minimal web server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::SocketAddr;
+
+#[tokio::main]
+async fn main() {
+    let port = 8080;
+    let addr = SocketAddr::from(([0, 0, 0, 0], port));
+    let listener = tokio::net::TcpListener::bind(addr)
+        .await
+        .unwrap_or_else(|e| panic!("failed to bind to {}: {}", addr, e));
+
+    let router = axum::Router::new();
+
+    println!("Listening on {}", listener.local_addr().unwrap());
+
+    axum::serve(listener, router)
+        .await
+        .unwrap_or_else(|e| panic!("failed to start server: {}", e));
 }
