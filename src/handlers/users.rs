d19614dac37e0ce13a5c001077289658947dc376
Edinson Jim <4842086+edinsonjim@users.noreply.github.com>
2025-09-23 20:57:54 +0200

    extract path parameters

--- src/handlers/users.rs
+++ src/handlers/users.rs
@@ -1,8 +1,13 @@
+use axum::extract::Path;
 use axum::response::IntoResponse;
 
 pub async fn create_user() -> impl IntoResponse {}
 pub async fn read_users() -> impl IntoResponse {}
-pub async fn read_user() -> impl IntoResponse {}
+
+pub async fn read_user(Path(user_id): Path<u32>) -> impl IntoResponse {
+    format!("fetching user with id: {}", user_id)
+}
+
 pub async fn update_user() -> impl IntoResponse {}
 pub async fn delete_user() -> impl IntoResponse {}
 pub async fn partial_update_user() -> impl IntoResponse {}
