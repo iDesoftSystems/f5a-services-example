e6b41f0980ff109c283da7b034a5e12e693aa1ae
Edinson Jim <4842086+edinsonjim@users.noreply.github.com>
2025-09-29 01:30:03 +0200

    apply all migrations

--- migration/src/lib.rs
+++ migration/src/lib.rs
@@ -1,10 +1,12 @@
 pub use sea_orm_migration::prelude::*;
 
+mod m20250928_230740_create_user_table;
+
 pub struct Migrator;
 
 #[async_trait::async_trait]
 impl MigratorTrait for Migrator {
     fn migrations() -> Vec<Box<dyn MigrationTrait>> {
-        vec![]
+        vec![Box::new(m20250928_230740_create_user_table::Migration)]
     }
 }
