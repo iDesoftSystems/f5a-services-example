5debd954b38f12c88162ba9f8c8c61f9775719be
Edinson Jim <4842086+edinsonjim@users.noreply.github.com>
2026-04-19 14:44:50 +0200

    refactor: update user migrations and pagination tests with new user creation logic

--- tests/users/migrations.rs
+++ tests/users/migrations.rs
@@ -1,16 +1,19 @@
-use sea_orm::{ActiveModelTrait, ActiveValue, ConnectionTrait, DbErr, sqlx::types::chrono::Utc};
+use chrono::NaiveDate;
+use sea_orm::{ActiveModelTrait, ActiveValue, ConnectionTrait, DbErr};
 
 pub async fn insert_idesoft_user(
     conn: &impl ConnectionTrait,
 ) -> Result<schemas::user::ActiveModel, DbErr> {
-    let created_at = Utc::now();
+    let created_at = NaiveDate::from_ymd_opt(2026, 03, 19)
+        .and_then(|date| date.and_hms_opt(10, 10, 10))
+        .unwrap();
 
     schemas::user::ActiveModel {
         id: ActiveValue::NotSet,
         username: ActiveValue::Set("idesoftd".into()),
         password: ActiveValue::Set("idesoftd".into()),
         disabled: ActiveValue::Set(true.into()),
-        created_at: ActiveValue::Set(created_at.naive_utc()),
+        created_at: ActiveValue::Set(created_at),
         creator_id: ActiveValue::Set(1),
     }
     .save(conn)
@@ -20,16 +23,39 @@ pub async fn insert_idesoft_user(
 pub async fn insert_blue_bird_user(
     conn: &impl ConnectionTrait,
 ) -> Result<schemas::user::ActiveModel, DbErr> {
-    let created_at = Utc::now();
+    let created_at = NaiveDate::from_ymd_opt(2026, 03, 19)
+        .and_then(|date| date.and_hms_opt(10, 10, 10))
+        .unwrap();
 
     schemas::user::ActiveModel {
         id: ActiveValue::NotSet,
         username: ActiveValue::Set("bluebird".into()),
         password: ActiveValue::Set("bluebird".into()),
         disabled: ActiveValue::Set(false.into()),
-        created_at: ActiveValue::Set(created_at.naive_utc()),
+        created_at: ActiveValue::Set(created_at),
         creator_id: ActiveValue::Set(1),
     }
     .save(conn)
     .await
+}
+
+pub async fn insert_chameleon_user(
+    conn: &impl ConnectionTrait,
+) -> Result<schemas::user::Model, DbErr> {
+    let created_at = NaiveDate::from_ymd_opt(2025, 12, 19)
+        .and_then(|date| date.and_hms_opt(10, 10, 10))
+        .unwrap();
+
+    let model = schemas::user::ActiveModel {
+        id: ActiveValue::NotSet,
+        username: ActiveValue::Set("chameleon".into()),
+        password: ActiveValue::Set("chameleon".into()),
+        disabled: ActiveValue::Set(false.into()),
+        created_at: ActiveValue::Set(created_at),
+        creator_id: ActiveValue::Set(1),
+    }
+    .insert(conn)
+    .await?;
+
+    Ok(model)
 }
--- tests/users/paginated.rs
+++ tests/users/paginated.rs
@@ -3,39 +3,77 @@ use axum::{
     http::{self, Request, StatusCode},
 };
 use f5a_services::users::http::om::UserPage;
+use serde_json::json;
 use tower::ServiceExt;
 
-use crate::{setup::TestContext, test_ext::IntoValue, users::migrations};
+use crate::{
+    setup::TestContext,
+    test_ext::IntoValue,
+    users::migrations::{insert_blue_bird_user, insert_chameleon_user, insert_idesoft_user},
+};
 
 #[tokio::test]
 async fn it_reads_paginated_users() {
     let ctx = TestContext::new().await;
     ctx.setup_schema().await;
 
-    migrations::insert_idesoft_user(ctx.db.as_ref())
-        .await
-        .unwrap();
-    migrations::insert_blue_bird_user(ctx.db.as_ref())
-        .await
-        .unwrap();
+    insert_idesoft_user(ctx.db.as_ref()).await.unwrap();
+    insert_blue_bird_user(ctx.db.as_ref()).await.unwrap();
+    insert_chameleon_user(ctx.db.as_ref()).await.unwrap();
 
-    let app = ctx.configure();
-
-    let req = Request::get("/api/users?page=0&page_size=10")
+    // assert for page 0
+    let app: axum::Router = ctx.configure();
+    let req = Request::get("/api/users?page=0&page_size=1")
         .header(http::header::CONTENT_TYPE, mime::APPLICATION_JSON.as_ref())
         .body(Body::empty())
         .unwrap();
+    let res = app.oneshot(req).await.unwrap();
+    assert_eq!(res.status(), StatusCode::OK);
+    let value = res.into_value::<Vec<UserPage>>().await;
+    assert_eq!(value.len(), 1);
+    assert_eq!(value[0].username, "chameleon");
+    assert_eq!(value[0].id, 3);
+    assert_eq!(value[0].disabled, false);
 
+    // assert for page 1
+    let app: axum::Router = ctx.configure();
+    let req = Request::get("/api/users?page=1&page_size=1")
+        .header(http::header::CONTENT_TYPE, mime::APPLICATION_JSON.as_ref())
+        .body(Body::empty())
+        .unwrap();
+    let res = app.oneshot(req).await.unwrap();
+    assert_eq!(res.status(), StatusCode::OK);
+    let value = res.into_value::<Vec<UserPage>>().await;
+    assert_eq!(value.len(), 1);
+    assert_eq!(value[0].username, "bluebird");
+    assert_eq!(value[0].id, 2);
+    assert_eq!(value[0].disabled, false);
+
+    // assert for page 2
+    let app: axum::Router = ctx.configure();
+    let req = Request::get("/api/users?page=2&page_size=1")
+        .header(http::header::CONTENT_TYPE, mime::APPLICATION_JSON.as_ref())
+        .body(Body::empty())
+        .unwrap();
+    let res = app.oneshot(req).await.unwrap();
+    assert_eq!(res.status(), StatusCode::OK);
+    let value = res.into_value::<Vec<UserPage>>().await;
+    assert_eq!(value.len(), 1);
+    assert_eq!(value[0].username, "idesoft");
+    assert_eq!(value[0].id, 1);
+    assert_eq!(value[0].disabled, true);
+
+    // assert for page 3
+    let app: axum::Router = ctx.configure();
+    let req = Request::get("/api/users?page=3&page_size=1")
+        .header(http::header::CONTENT_TYPE, mime::APPLICATION_JSON.as_ref())
+        .body(Body::empty())
+        .unwrap();
     let res = app.oneshot(req).await.unwrap();
     assert_eq!(res.status(), StatusCode::OK);
 
     let value = res.into_value::<Vec<UserPage>>().await;
-    assert_eq!(value.len(), 2);
-
-    assert_eq!(value[0].username, "bluebird");
-    assert_eq!(value[0].disabled, false);
-    assert_eq!(value[1].username, "idesoftd");
-    assert_eq!(value[1].disabled, true);
+    assert_eq!(value.len(), 0);
 }
 
 #[tokio::test]
@@ -55,4 +93,34 @@ async fn it_reads_empty_paginated_users() {
 
     let value = res.into_value::<Vec<UserPage>>().await;
     assert_eq!(value.len(), 0);
+}
+
+#[tokio::test]
+async fn it_reads_paginated_users_with_idiomatic_json() {
+    let ctx = TestContext::new().await;
+    ctx.setup_schema().await;
+
+    insert_idesoft_user(ctx.db.as_ref()).await.unwrap();
+
+    let app: axum::Router = ctx.configure();
+    let req = Request::get("/api/users?page=0&page_size=1")
+        .header(http::header::CONTENT_TYPE, mime::APPLICATION_JSON.as_ref())
+        .body(Body::empty())
+        .unwrap();
+
+    let res = app.oneshot(req).await.unwrap();
+    assert_eq!(res.status(), StatusCode::OK);
+
+    let value = res.into_value::<serde_json::Value>().await;
+    let expected_body = json!([
+        {
+            "id": 1,
+            "username": "idesoft",
+            "fullName": "iDesoft Systems",
+            "disabled": 1,
+            "createdAt": "2026-03-19T10:10:10Z"
+        }
+    ]);
+
+    assert_eq!(value, expected_body);
 }
